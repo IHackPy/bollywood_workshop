8e24e9fa083fb04b8eb85e9a49b160f38e9a0e09
Marc Garcia <garcia.marc@gmail.com>
2018-09-09 00:09:40 +0100

    Spider should be getting all the information from the movie list. Notebooks renamed

--- scraping/bollywood/bollywood/spiders/imdb.py
+++ scraping/bollywood/bollywood/spiders/imdb.py
@@ -1,4 +1,8 @@
+import urllib.parse
 import scrapy
+
+
+BASE_URL = 'https://www.imdb.com/search/title'
 
 
 class ImdbSpider(scrapy.Spider):
@@ -6,19 +10,54 @@ class ImdbSpider(scrapy.Spider):
     allowed_domains = ['imdb.com']
 
     def start_requests(self):
-        # 197
-        for page in range(1, 197):
-            yield scrapy.Request('https://www.imdb.com/search/title?title_type=feature&release_date=1970-01-01,&countries=in&languages=hi&page={}'.format(page),
-                                 callback=self.parse_list)
+        params = {'title_type': 'feature',
+                  'release_date': '1970-01-01,',
+                  'countries': 'in',
+                  'languages': 'hi',
+                  'page': 1}
+        url = '{}?{}'.format(BASE_URL, urllib.parse.urlencode(params))
+        yield scrapy.Request(url, callback=self.parse_list)
 
     def parse_list(self, response):
-        for item in response.css('h3.lister-item-header'):
-            text = item.css('a::text').extract_first()
-            link = item.css('a::attr(href)').extract_first()
-            id_ = item.css('a::attr(href)').re('/title/(tt\d+)/.*')[0]
-            yield {'title': text,
-                   'id': id_}
-            #yield response.follow(link, self.parse_movie)
+        """
+        Extract information from the list of movies.
+
+        With the current selection of India / Hindi, there are 197 page, with
+        50 movies in each, for a total of 9,852 movies.
+        """
+        for item in response.css('div.lister-item'):
+            votes_and_gross = item.css('div.lister-item-content span[name="nv"]::attr(data-value)').extract()
+            votes = votes_and_gross[0] if votes_and_gross else None
+            gross = votes_and_gross[1] if len(votes_and_gross) > 1 else None
+
+            cast_selector = item.css('div.lister-item-content p')[2].css('a')
+            cast = []
+            for person in cast_selector:
+                cast.append({'name': person.css('::text').extract_first(),
+                             'link': person.css('::attr(href)').extract_first()})
+
+            genre = item.css('div.lister-item-content span.genre::text').extract_first()
+            if genre is not None:
+                genre = genre.strip()
+
+            yield {
+                'imdb_id': item.css('h3.lister-item-header a::attr(href)').re('/title/(tt\d+)/.*')[0],
+                'link': item.css('h3.lister-item-header a::attr(href)').extract_first(),
+                'title': item.css('h3.lister-item-header a::text').extract_first(),
+                'image': item.css('div.lister-item-image img::attr(src)').extract_first(),
+                'year': item.css('h3.lister-item-header span.lister-item-year::text').extract_first(),
+                'certificate': item.css('div.lister-item-content span.certificate::text').extract_first(),
+                'runtime': item.css('div.lister-item-content span.runtime::text').extract_first(),
+                'genre': genre,
+                'rating': item.css('div.ratings-bar div[name="ir"]::attr(data-value)').extract_first(),
+                'summary': item.css('div.lister-item-content p.text-muted')[1].css('::text').extract_first().strip(),
+                'votes': votes,
+                'gross': gross[0] if gross else None,
+                'cast': cast,
+            }
+        next_page = response.css('div.desc a.lister-page-next::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse_list)
 
     def parse_movie(self, response):
         pass
