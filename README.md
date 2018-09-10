35bca8d993791148929763cbb35c5cb6898d2744
Marc Garcia <marc.garcia1@tesco.com>
2018-09-10 16:13:47 +0100

    Minor fixes

--- README.md
+++ README.md
@@ -7,13 +7,16 @@ workshop presented at PyCon India 2018.
 Installation
 ------------
 
-To be able to install and run the files, you need to follow the next steps:
+To run the materials during the workshop, you need a Jupyter notebook
+with recent versions of pandas, matplotlib, bokeh, gensim and scikit-learn.
+
+The set up instructions to create such an environment are listed next.
 
 - Download [Miniconda](https://conda.io/miniconda.html) for Python 3.6 and your architecture (probably 64-bits)
 - Add Miniconda path to your system path: `export PATH=<path-to-miniconda>:$PATH`
 - Clone repository: `git clone git@github.com:datapythonista/bollywood_workshop.git`
 - Change to the repository directory: `cd bollywood_workshop`
-- Create your conda environment: `conda env create -f requirements.yaml`
+- Create your conda environment: `conda env create`
 - Activate the environment: `source activate bollywood`
 - Change to the notebooks directory: `cd notebooks`
 - Run the jupyter notebook server: `jupyter notebook`
@@ -21,8 +24,9 @@ To be able to install and run the files, you need to follow the next steps:
 scraping
 --------
 
-The scraping subproject is a `scrapy` project that obtains the main datasets
-from IMBd.
+The scraping subproject is not presented during the workshop, but its source it's
+included in this repo. It is a `scrapy` project that obtains the main datasets used
+during the workshop from the Internet Movie Database (imdb.com).
 
 To run the project
 
--- scraping/bollywood/bollywood/spiders/imdb.py
+++ scraping/bollywood/bollywood/spiders/imdb.py
@@ -2,7 +2,7 @@ import urllib.parse
 import scrapy
 
 
-BASE_URL = 'https://www.imdb.com/search/title'
+BASE_URL = 'https://www.imdb.com'
 
 
 class ImdbSpider(scrapy.Spider):
@@ -15,7 +15,8 @@ class ImdbSpider(scrapy.Spider):
                   'countries': 'in',
                   'languages': 'hi',
                   'page': 1}
-        url = '{}?{}'.format(BASE_URL, urllib.parse.urlencode(params))
+        url = '{}/search/title?{}'.format(BASE_URL,
+                                          urllib.parse.urlencode(params))
         yield scrapy.Request(url, callback=self.parse_list)
 
     def parse_list(self, response):
@@ -26,7 +27,7 @@ class ImdbSpider(scrapy.Spider):
         50 movies in each, for a total of 9,852 movies.
         """
         for item in response.css('div.lister-item'):
-            votes_and_gross = item.css('div.lister-item-content span[name="nv"]::attr(data-value)').extract()
+            votes_and_gross = item.css('div.lister-item-content span[name="nv"]::attr(data-value)').extract()  # noqa: E501
             votes = votes_and_gross[0] if votes_and_gross else None
             gross = votes_and_gross[1] if len(votes_and_gross) > 1 else None
 
@@ -35,28 +36,31 @@ class ImdbSpider(scrapy.Spider):
             cast = []
             for person in cast_selector:
                 cast.append({'name': person.css('::text').extract_first(),
-                             'link': person.css('::attr(href)').extract_first()})
+                             'link': person.css('::attr(href)').extract_first()})  # noqa: E501
 
-            genre = item.css('div.lister-item-content span.genre::text').extract_first()
+            genre = item.css('div.lister-item-content span.genre::text').extract_first()  # noqa: E501
             if genre is not None:
                 genre = genre.strip()
 
+            # TODO get partial selectors to make the next code simpler/shorter:
+            # >>> item_header = item.css('h3.lister-item-header')
+            # >>> link = item_header.css('a::attr(href)').extract_first()
             yield {
-                'imdb_id': item.css('h3.lister-item-header a::attr(href)').re('/title/(tt\d+)/.*')[0],
-                'link': item.css('h3.lister-item-header a::attr(href)').extract_first(),
-                'title': item.css('h3.lister-item-header a::text').extract_first(),
-                'image': item.css('div.lister-item-image img::attr(src)').extract_first(),
-                'year': item.css('h3.lister-item-header span.lister-item-year::text').extract_first(),
-                'certificate': item.css('div.lister-item-content span.certificate::text').extract_first(),
-                'runtime': item.css('div.lister-item-content span.runtime::text').extract_first(),
+                'imdb_id': item.css('h3.lister-item-header a::attr(href)').re('/title/(tt\d+)/.*')[0],  # noqa: E501
+                'link': item.css('h3.lister-item-header a::attr(href)').extract_first(),  # noqa: E501
+                'title': item.css('h3.lister-item-header a::text').extract_first(),  # noqa: E501
+                'image': item.css('div.lister-item-image img::attr(src)').extract_first(),  # noqa: E501
+                'year': item.css('h3.lister-item-header span.lister-item-year::text').extract_first(),  # noqa: E501
+                'certificate': item.css('div.lister-item-content span.certificate::text').extract_first(),  # noqa: E501
+                'runtime': item.css('div.lister-item-content span.runtime::text').extract_first(),  # noqa: E501
                 'genre': genre,
-                'rating': item.css('div.ratings-bar div[name="ir"]::attr(data-value)').extract_first(),
-                'summary': item.css('div.lister-item-content p.text-muted')[1].css('::text').extract_first().strip(),
+                'rating': item.css('div.ratings-bar div[name="ir"]::attr(data-value)').extract_first(),  # noqa: E501
+                'summary': item.css('div.lister-item-content p.text-muted')[1].css('::text').extract_first().strip(),  # noqa: E501
                 'votes': votes,
                 'gross': gross[0] if gross else None,
                 'cast': cast,
             }
-        next_page = response.css('div.desc a.lister-page-next::attr(href)').extract_first()
+        next_page = response.css('div.desc a.lister-page-next::attr(href)').extract_first()  # noqa: E501
         if next_page is not None:
             yield response.follow(next_page, self.parse_list)
 
